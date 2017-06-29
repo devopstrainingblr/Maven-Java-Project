4517a66b715764aaae16194459cbc7ec9813cfd1
Bhaskar Reddy L <bhlaccha@in.ibm.com>
2017-06-29 07:24:43 +0530

    Initial Repo

---
+++ src/main/java/com/bt/maven/java/project/App.java
@@ -0,0 +1,14 @@
+package com.bt.maven.java.project;
+
+/**
+ * Hello world!
+ *
+ */
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println("Hello World!");
+    }
+}
